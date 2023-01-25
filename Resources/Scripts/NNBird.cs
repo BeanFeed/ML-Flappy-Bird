aa20d878751f0ccb05307a8db4cf4936c22152e5
BeanFeed <two.austin.two@gmail.com>
2023-01-24 22:21:21 -0500

    blank bird classes

--- Resources/Scripts/NNBird.cs
+++ Resources/Scripts/NNBird.cs
@@ -5,71 +5,12 @@ using System.Collections.Generic;
 
 public class NNBird : Bird
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    //private List<int> pastPipes = new List<int>();
-    public NeuralNetwork Brain = new NeuralNetwork(new int[]{1,10,1});
-    public bool dead = false;
-    private BirdSpawner spawner = null;
-    // Called when the node enters the scene tree for the first time.
-
-    
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Ready()
+    {
+        
+    }
     public override void _Process(float delta)
     {
 
-        base._Process(delta);
-        var simGoVal = GetNode<Level>("/root/Level").simGo;
-        if (!GetNode<Level>("/root/Level").simGo || dead) return;
-        GD.Print(GetNode<RayCast2D>("GroundDetector").GetCollider() != null);
-        if (GetNode<RayCast2D>("GroundDetector").GetCollider() != null) Flap();
-        if(PipeInFront())
-        {
-            var nextPipe = GetNextPipe();
-            var pipeY = nextPipe.Position.y;
-
-            Brain.SetInput(new float[]{pipeY - Position.y});
-        }
-        
-        Brain.Process();
-        float[] results = Brain.getOutput();
-        if(results[0]>0.5f) Flap();
-    }
-
-    private Pipe GetNextPipe()
-    {
-        var ray = GetNode<RayCast2D>("NextPipeDetector");
-        var obj = ray.GetCollider();
-        if (obj is Pipe p) return p;
-        else return null;
-    }
-
-    private bool PipeInFront()
-    {
-        var ray = GetNode<RayCast2D>("NextPipeDetector");
-        var obj = ray.GetCollider();
-        if(obj != null && obj is Pipe) return true;
-        return false;
-    }
-    
-    private protected override void Kill()
-    {
-        dead = true;
-        spawner.CheckAllDead();
-    }
-    
-    public void ReturnStats()
-    {
-        if (spawner.bestScore < score)
-        {
-            spawner.bestScore = score;
-            spawner.bestModel = Brain.GeAllWeights();
-        }
-    }
-
-    public void SetSpawner(BirdSpawner spawner)
-    {
-        this.spawner = spawner;
     }
 }
--- Resources/Scripts/PlayerBird.cs
+++ Resources/Scripts/PlayerBird.cs
@@ -4,30 +4,12 @@ using System;
 
 class PlayerBird : Bird
 {
-    private bool dead = false;
+    public override void _Ready()
+    {
+        
+    }
     public override void _Process(float delta)
     {
-        base._Process(delta);
-        HandleInputs();
-        if(dead) LinearVelocity = Vector2.Zero;
-    }
 
-    private protected override void Score(int pipeID)
-    {
-        base.Score(pipeID);
-        var label = GetNode<Label>("/root/Level/UI/Score");
-        label.Text = "Best Score: " + score;
-    }
-    private void HandleInputs()
-    {
-        var level = GetNode<Level>("/root/Level");
-        if(Input.IsActionJustPressed("Jump") && level.simGo) this.Flap();
-    }
-
-    private protected override void Kill()
-    {
-        var level = GetNode<Level>("/root/Level");
-        level.simGo = false;
-        dead = true;
     }
 }
